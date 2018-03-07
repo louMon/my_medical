1ef310fb9b43440e68ace274b34f2baf5e8d1508
Lourdes <montalvo.lourdes@pucp.edu.pe>
2018-03-07 14:53:23 -0500

    begin again cause I have been learned

--- app/models/specialty.rb
+++
@@ -1,6 +0,0 @@
-class Specialty
-  include Mongoid::Document
-  field :name, type: String
-  field :description, type: String
-  embeds_one :doctor
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  get 'doctors/index'
-
-  get 'doctors/new'
-
-  get 'doctors/create'
-
-  get 'doctors/edit'
-
-  get 'doctors/update'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
